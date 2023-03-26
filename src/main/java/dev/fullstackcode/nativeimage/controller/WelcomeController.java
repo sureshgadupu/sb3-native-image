4e31a1499b6e71458a70a88faa03e3dec71c2d9f
sureshgadupu <sureshgkhyd@gmail.com>
2023-03-26 19:25:20 +1300

    Spring Boot Native Image

---
+++ src/main/java/dev/fullstackcode/nativeimage/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package dev.fullstackcode.nativeimage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @RequestMapping("/hello")
+    public String home() {
+        return "Hello World!";
+    }
+
+}
